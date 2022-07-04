e9e29577eb5d81a7e303e027ddab7b4191239f96
Leo <100171318+leoaversa@users.noreply.github.com>
2022-07-03 23:07:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,24 @@ const contenedorProductos = document.getElementById('contenedor-productos')
 
 const contenedorCarrito = document.getElementById('productoEnCarrito')
 
+const contadorCarrito = document.getElementById('contador-carrito')
+
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+const precioTotal = document.getElementById('precioTotal')
+
+
+
 
 let carrito = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+}) 
+
 
 stockProductos.forEach((producto) => {
 
@@ -30,8 +46,8 @@ stockProductos.forEach((producto) => {
             icon: 'success',
             title: 'Agregaste al carrito',
             text: ("Adidas:" + " " + producto.modelo),
-            showConfirmButton: false,
-            timer: 1500
+            showConfirmButton: true,
+            timer: 2000
           })
 
         
@@ -43,29 +59,85 @@ const agregarAlCarrito = (prodId) => {
 
     const item = stockProductos.find((prod) => prod.id === prodId)
     carrito.push(item)
-    /* actualizarCarrito() */
+    actualizarCarrito()
     console.log(carrito)
     
 }
 
+const elliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarrito()
+
+}
+
+
+
+
 const actualizarCarrito = () => {
-    /* contenedorCarrito.innerHTML = "" */
+    contenedorCarrito.innerHTML = ""
 
     carrito.forEach((prod) => {
 
         const div = document.createElement('div')
-        div.className("productoEnCarrito")
-        div.innerHTML = `
+        div.classList.add("formulario-carrito", "mt-5", "container");
+        div.innerHTML += `
+        <img src=${prod.img} alt="">
         <p>${prod.modelo}</p>
         <p>Precio: ${prod.precio}</p>
         <p>Cantidad: <span id= "cantidad">${prod.cantidad}</span></p>
-        <button onclick = "eliminarDelCarrito(${prod.id})" class= 'boton-eliminar'><i class='fas fa-trash-alt'></i></button>
+        <button onclick= "eliminarDelCarrito(${prod.id})" class= 'btn boton-eliminar btn-primary'><i class='fas fa-trash-alt'></i>Eliminar</button>
         `
         
-
         contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem ('carrito', JSON.stringify(carrito))
+
+        //SUMAR PRECIOS EN CARRITO
+        
+        const total = carrito.map((prod)=> (prod.precio)).reduce((carritoPrecioTotal,
+        carritoPrecioActual) => carritoPrecioTotal + carritoPrecioActual, 0);
+        console.log(total)
+        
+        precioTotal.innerText = (total)
+
+        botonVaciar.onclick = () => {
+            carrito.length = 0
+            div.innerHTML = ``
+            console.log(carrito)            
+            actualizarCarrito()
+        
+        }
+
+        
+
+        //CONTADOR CARRITO DEL NAV
+        contadorCarrito.innerText = (carrito.length)
+        
     })
 }
+
+        const buscador = document.getElementById('buscador')
+
+        function filtrarPorCat(){
+            const filtrarproductos = productos.filter((prod) => prod.categoría === buscador.value)
+            console.log(filtrarproductos)
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
